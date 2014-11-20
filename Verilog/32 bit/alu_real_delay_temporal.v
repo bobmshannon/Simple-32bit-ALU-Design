4a705e211d956f6910cedf82ab98b8ab8d9f72d6
U-Bobby-PC\Bobby <Bobby@Bobby-PC.(none)>
2014-11-19 21:21:56 -0500

    Added new alu files

--- Verilog/32 bit/alu_real_delay_temporal.v
+++ Verilog/32 bit/alu_real_delay_temporal.v
@@ -3,6 +3,7 @@
 /*                        Author: Robert Shannon                    */
 /*                       E-mail: rshannon@buffalo.edu               */
 /* **************************************************************** */
+`timescale 1ns/1ns
 
 /*
 /* ========================================================= */
@@ -37,78 +38,9 @@ module testALU(x, y, opcode, f, overflow, cout, zero);
 		$display("----------------------------------------------------------");
 		$display("                 ADDITION FUNCTIONALITY TEST              ");
 		$display("----------------------------------------------------------");
-		#20 x=1024; y=128; opcode=0; #20
-		$display("1. x=%d, y=%d, opcode=%b, f=%d, overflow=%b, zero=%d, cout=%b",x,y,opcode,f,overflow, zero,cout);
-		#20 x=100837; y=385421; opcode=0; #20
-		$display("2. x=%d, y=%d, opcode=%b, f=%d, overflow=%b, zero=%d, cout=%b",x,y,opcode,f,overflow, zero,cout);
-		#20 x=37; y=6; opcode=0; #20
-		$display("3. x=%d, y=%d, opcode=%b, f=%d, overflow=%b, zero=%d, cout=%b",x,y,opcode,f,overflow, zero,cout);
-		#20 x=842; y=986; opcode=0; #20
-		$display("4. x=%d, y=%d, opcode=%b, f=%d, overflow=%b, zero=%d, cout=%b",x,y,opcode,f,overflow, zero,cout);
-		#20 x=1000000000; y=3000000000; opcode=0; #20
-		$display("5. x=%d, y=%d, opcode=%b, f=%d, overflow=%b, zero=%d, cout=%b",x,y,opcode,f,overflow, zero,cout);
-		
-		// SUB
-		$display("----------------------------------------------------------");
-		$display("              SUBTRACTION FUNCTIONALITY TEST              ");
-		$display("      NOTE: Result is in 2's complement representation.   ");
-		$display("----------------------------------------------------------");
-		#20 x=8108; y=9375; opcode=3; #20
-		$display("1. x=%d, y=%d, opcode=%b, f=%b, overflow=%b, zero=%d, cout=%b",x,y,opcode,f,overflow, zero,cout);
-		#20 x=3264; y=2917; opcode=3; #20
-		$display("2. x=%d, y=%d, opcode=%b, f=%b, overflow=%b, zero=%d, cout=%b",x,y,opcode,f,overflow, zero,cout);
-		#20 x=121; y=161; opcode=3; #20
-		$display("3. x=%d, y=%d, opcode=%b, f=%b, overflow=%b, zero=%d, cout=%b",x,y,opcode,f,overflow, zero,cout);
-		#20 x=842; y=986; opcode=3; #20
-		$display("4. x=%d, y=%d, opcode=%b, f=%b, overflow=%b, zero=%d, cout=%b",x,y,opcode,f,overflow, zero,cout);
-		#20 x=95; y=34; opcode=3; #20
-		$display("5. x=%d, y=%d, opcode=%b, f=%b, overflow=%b, zero=%d, cout=%b",x,y,opcode,f,overflow, zero,cout);
-		
-		// BITWISE AND
-		$display("----------------------------------------------------------");
-		$display("              BITWISE AND FUNCTIONALITY TEST              ");
-		$display("----------------------------------------------------------");
-		#20 x=32'b11010111100100011101011011110011; y=32'b10111111011000000010000101110001; opcode=2; #20
-		$display("1. x=%b, y=%b, opcode=%b, f=%b, overflow=%b, zero=%d, cout=%b",x,y,opcode,f,overflow, zero,cout);	
-		#20 x=32'b00111000001011011000000111111010; y=32'b10001110100111011010100001001111; opcode=2; #20
-		$display("2. x=%b, y=%b, opcode=%b, f=%b, overflow=%b, zero=%d, cout=%b",x,y,opcode,f,overflow, zero,cout);
-		#20 x=32'b00100011100011101000111010110101; y=32'b10111101011001001101011111110110; opcode=2; #20
-		$display("3. x=%b, y=%b, opcode=%b, f=%b, overflow=%b, zero=%d, cout=%b",x,y,opcode,f,overflow, zero,cout);
-		#20 x=32'b10101100010010010100011000111011; y=32'b10001101100011101101001110011101; opcode=2; #20
-		$display("4. x=%b, y=%b, opcode=%b, f=%b, overflow=%b, zero=%d, cout=%b",x,y,opcode,f,overflow, zero,cout);
-		#20 x=32'b10101011100100001101011000001011; y=32'b00010100011110101110000000010011; opcode=2; #20	
-		$display("5. x=%b, y=%b, opcode=%b, f=%b, overflow=%b, zero=%d, cout=%b",x,y,opcode,f,overflow, zero,cout);	
-		
-		// BITWISE OR
-		$display("----------------------------------------------------------");
-		$display("               BITWISE OR FUNCTIONALITY TEST              ");
-		$display("----------------------------------------------------------");
-		#20 x=32'b01001010111011101101011101000100; y=32'b11001010111000111101100000001000; opcode=1; #20
-		$display("1. x=%b, y=%b, opcode=%b, f=%b, overflow=%b, zero=%d, cout=%b",x,y,opcode,f,overflow, zero,cout);	
-		#20 x=32'b00010110010000100000101010001111; y=32'b11001111111010000100011000111011; opcode=1; #20
-		$display("2. x=%b, y=%b, opcode=%b, f=%b, overflow=%b, zero=%d, cout=%b",x,y,opcode,f,overflow, zero,cout);
-		#20 x=32'b11001111100000001111100011010000; y=32'b00011010100011100100011100000011; opcode=1; #20
-		$display("3. x=%b, y=%b, opcode=%b, f=%b, overflow=%b, zero=%d, cout=%b",x,y,opcode,f,overflow, zero,cout);	
-		#20 x=32'b10111101111011000011100011101110; y=32'b11101011110011110011111101010100; opcode=1; #20
-		$display("4. x=%b, y=%b, opcode=%b, f=%b, overflow=%b, zero=%d, cout=%b",x,y,opcode,f,overflow, zero,cout);
-		#20 x=32'b00111011010100010110110001010000; y=32'b11010101000110011100110110011101; opcode=1; #20
-		$display("5. x=%b, y=%b, opcode=%b, f=%b, overflow=%b, zero=%d, cout=%b",x,y,opcode,f,overflow, zero,cout);
-		
-		// BITWISE OR
-		$display("----------------------------------------------------------");
-		$display("          SET LESS THAN (SLT) FUNCTIONALITY TEST          ");
-		$display("----------------------------------------------------------");
-		#20 x=24; y=65; opcode=4; #20
-		$display("1. x=%d, y=%d, opcode=%b, f=%d, overflow=%b, zero=%d, cout=%b",x,y,opcode,f,overflow, zero,cout);
-		#20 x=71; y=57; opcode=4; #20
-		$display("2. x=%d, y=%d, opcode=%b, f=%d, overflow=%b, zero=%d, cout=%b",x,y,opcode,f,overflow, zero,cout);
-		#20 x=14507; y=97400; opcode=4; #20
-		$display("3. x=%d, y=%d, opcode=%b, f=%d, overflow=%b, zero=%d, cout=%b",x,y,opcode,f,overflow, zero,cout);
-		#20 x=971015; y=167060; opcode=4; #20
-		$display("4. x=%d, y=%d, opcode=%b, f=%d, overflow=%b, zero=%d, cout=%b",x,y,opcode,f,overflow, zero,cout);
-		#20 x=4026855; y=1301126; opcode=4;	#20
-		$display("5. x=%d, y=%d, opcode=%b, f=%b, overflow=%b, zero=%d, cout=%b",x,y,opcode,f,overflow, zero,cout);
-		
+		x=1024; y=128; opcode=3;
+		$monitor ($time,,"1. x=%d, y=%d, opcode=%b, f=%d, overflow=%b, zero=%d, cout=%b",x,y,opcode,f,overflow, zero,cout);
+
 		end
 endmodule
 
@@ -193,43 +125,43 @@ module iszero(f, zero);
 	input [31:0] f;
 	output zero;
 
-	or(w1,f[0],f[1]);
-	or(w2,f[2],f[3]);
-	or(w3,f[4],f[5]);
-	or(w4,f[6],f[7]);
-	or(w5,f[8],f[9]);
-	or(w6,f[10],f[11]);
-	or(w7,f[12],f[13]);
-	or(w8,f[14],f[15]);
-	or(w9,f[16],f[17]);
-	or(w10,f[18],f[19]);
-	or(w11,f[20],f[21]);
-	or(w12,f[22],f[23]);
-	or(w13,f[24],f[25]);
-	or(w14,f[26],f[27]);
-	or(w15,f[28],f[29]);
-	or(w16,f[30],f[31]);
+	or #6(w1,f[0],f[1]);
+	or #6(w2,f[2],f[3]);
+	or #6(w3,f[4],f[5]);
+	or #6(w4,f[6],f[7]);
+	or #6(w5,f[8],f[9]);
+	or #6(w6,f[10],f[11]);
+	or #6(w7,f[12],f[13]);
+	or #6(w8,f[14],f[15]);
+	or #6(w9,f[16],f[17]);
+	or #6(w10,f[18],f[19]);
+	or #6(w11,f[20],f[21]);
+	or #6(w12,f[22],f[23]);
+	or #6(w13,f[24],f[25]);
+	or #6(w14,f[26],f[27]);
+	or #6(w15,f[28],f[29]);
+	or #6(w16,f[30],f[31]);
 	
-	or(w17, w1, w2);
-	or(w18, w3, w4);
-	or(w19, w5, w6);
-	or(w20, w7, w8);
-	or(w21, w9, w10);
-	or(w22, w11, w12);
-	or(w23, w13, w14);
-	or(w24, w15, w16);
+	or #6(w17, w1, w2);
+	or #6(w18, w3, w4);
+	or #6(w19, w5, w6);
+	or #6(w20, w7, w8);
+	or #6(w21, w9, w10);
+	or #6(w22, w11, w12);
+	or #6(w23, w13, w14);
+	or #6(w24, w15, w16);
 	
-	or(w25, w17, w18);
-	or(w26, w19, w20);
-	or(w27, w21, w22);
-	or(w28, w23, w24);
+	or #6(w25, w17, w18);
+	or #6(w26, w19, w20);
+	or #6(w27, w21, w22);
+	or #6(w28, w23, w24);
 	
-	or(w29, w25, w26);
-	or(w30, w27, w28);
+	or #6(w29, w25, w26);
+	or #6(w30, w27, w28);
 	
-	or(w31, w29, w30);
+	or #6(w31, w29, w30);
 	
-	not(zero, w31);
+	not #1(zero, w31);
 endmodule
 
 /*
@@ -284,14 +216,14 @@ module fulladder(a, b, c, s, cout);
 	input a, b, c;
 	output s, cout;
 
-	xor
+	xor #3
 		g1(w1, a, b),
 		g2(s, w1, c);
-	and
+	and #2
 		g3(w2, c, b),
 		g4(w3, c, a),
 		g5(w4, a, b);
-	or
+	or #6
 		g6(cout, w2, w3, w4);
 endmodule
 
@@ -311,43 +243,43 @@ module sub(x,y,f,overflow);
 	assign one = 32'b1;
 	assign zero = 1'b0;
 
-	not(ynot[0],y[0]);
-	not(ynot[1],y[1]);
-	not(ynot[2],y[2]);
-	not(ynot[3],y[3]);
-	not(ynot[4],y[4]);
-	not(ynot[5],y[5]);
-	not(ynot[6],y[6]);
-	not(ynot[7],y[7]);
-	not(ynot[8],y[8]);
-	not(ynot[9],y[9]);
-	not(ynot[10],y[10]);
-	not(ynot[11],y[11]);
-	not(ynot[12],y[12]);
-	not(ynot[13],y[13]);
-	not(ynot[14],y[14]);
-	not(ynot[15],y[15]);
-	not(ynot[16],y[16]);
-	not(ynot[17],y[17]);
-	not(ynot[18],y[18]);
-	not(ynot[19],y[19]);
-	not(ynot[20],y[20]);
-	not(ynot[21],y[21]);
-	not(ynot[22],y[22]);
-	not(ynot[23],y[23]);
-	not(ynot[24],y[24]);
-	not(ynot[25],y[25]);
-	not(ynot[26],y[26]);
-	not(ynot[27],y[27]);
-	not(ynot[28],y[28]);
-	not(ynot[29],y[29]);
-	not(ynot[30],y[30]);
-	not(ynot[31],y[31]);
+	not #1(ynot[0],y[0]);
+	not #1(ynot[1],y[1]);
+	not #1(ynot[2],y[2]);
+	not #1(ynot[3],y[3]);
+	not #1(ynot[4],y[4]);
+	not #1(ynot[5],y[5]);
+	not #1(ynot[6],y[6]);
+	not #1(ynot[7],y[7]);
+	not #1(ynot[8],y[8]);
+	not #1(ynot[9],y[9]);
+	not #1(ynot[10],y[10]);
+	not #1(ynot[11],y[11]);
+	not #1(ynot[12],y[12]);
+	not #1(ynot[13],y[13]);
+	not #1(ynot[14],y[14]);
+	not #1(ynot[15],y[15]);
+	not #1(ynot[16],y[16]);
+	not #1(ynot[17],y[17]);
+	not #1(ynot[18],y[18]);
+	not #1(ynot[19],y[19]);
+	not #1(ynot[20],y[20]);
+	not #1(ynot[21],y[21]);
+	not #1(ynot[22],y[22]);
+	not #1(ynot[23],y[23]);
+	not #1(ynot[24],y[24]);
+	not #1(ynot[25],y[25]);
+	not #1(ynot[26],y[26]);
+	not #1(ynot[27],y[27]);
+	not #1(ynot[28],y[28]);
+	not #1(ynot[29],y[29]);
+	not #1(ynot[30],y[30]);
+	not #1(ynot[31],y[31]);
 
 	add adder0(one,ynot,ynotplusone,overflow1,zero);
 	add adder1(x,ynotplusone,f,overflow2,zero);
 	
-	or(overflow, overflow1, overflow2);
+	or #6(overflow, overflow1, overflow2);
 endmodule
 
 /*
@@ -361,38 +293,38 @@ module bitwiseand(x,y,f);
 	input [31:0] x, y;
 	output [31:0] f;
 	
-	and(f[0],x[0],y[0]);
-	and(f[1],x[1],y[1]);
-	and(f[2],x[2],y[2]);
-	and(f[3],x[3],y[3]);
-	and(f[4],x[4],y[4]);
-	and(f[5],x[5],y[5]);
-	and(f[6],x[6],y[6]);
-	and(f[7],x[7],y[7]);
-	and(f[8],x[8],y[8]);
-	and(f[9],x[9],y[9]);
-	and(f[10],x[10],y[10]);
-	and(f[11],x[11],y[11]);
-	and(f[12],x[12],y[12]);
-	and(f[13],x[13],y[13]);
-	and(f[14],x[14],y[14]);
-	and(f[15],x[15],y[15]);
-	and(f[16],x[16],y[16]);
-	and(f[17],x[17],y[17]);
-	and(f[18],x[18],y[18]);
-	and(f[19],x[19],y[19]);
-	and(f[20],x[20],y[20]);
-	and(f[21],x[21],y[21]);
-	and(f[22],x[22],y[22]);
-	and(f[23],x[23],y[23]);
-	and(f[24],x[24],y[24]);
-	and(f[25],x[25],y[25]);
-	and(f[26],x[26],y[26]);
-	and(f[27],x[27],y[27]);
-	and(f[28],x[28],y[28]);
-	and(f[29],x[29],y[29]);
-	and(f[30],x[30],y[30]);
-	and(f[31],x[31],y[31]);
+	and #2(f[0],x[0],y[0]);
+	and #2(f[1],x[1],y[1]);
+	and #2(f[2],x[2],y[2]);
+	and #2(f[3],x[3],y[3]);
+	and #2(f[4],x[4],y[4]);
+	and #2(f[5],x[5],y[5]);
+	and #2(f[6],x[6],y[6]);
+	and #2(f[7],x[7],y[7]);
+	and #2(f[8],x[8],y[8]);
+	and #2(f[9],x[9],y[9]);
+	and #2(f[10],x[10],y[10]);
+	and #2(f[11],x[11],y[11]);
+	and #2(f[12],x[12],y[12]);
+	and #2(f[13],x[13],y[13]);
+	and #2(f[14],x[14],y[14]);
+	and #2(f[15],x[15],y[15]);
+	and #2(f[16],x[16],y[16]);
+	and #2(f[17],x[17],y[17]);
+	and #2(f[18],x[18],y[18]);
+	and #2(f[19],x[19],y[19]);
+	and #2(f[20],x[20],y[20]);
+	and #2(f[21],x[21],y[21]);
+	and #2(f[22],x[22],y[22]);
+	and #2(f[23],x[23],y[23]);
+	and #2(f[24],x[24],y[24]);
+	and #2(f[25],x[25],y[25]);
+	and #2(f[26],x[26],y[26]);
+	and #2(f[27],x[27],y[27]);
+	and #2(f[28],x[28],y[28]);
+	and #2(f[29],x[29],y[29]);
+	and #2(f[30],x[30],y[30]);
+	and #2(f[31],x[31],y[31]);
 endmodule
 
 /*
@@ -406,38 +338,38 @@ module bitwiseor(x,y,f);
 	input [31:0] x, y;
 	output [31:0] f;
 	
-	or(f[0],x[0],y[0]);
-	or(f[1],x[1],y[1]);
-	or(f[2],x[2],y[2]);
-	or(f[3],x[3],y[3]);
-	or(f[4],x[4],y[4]);
-	or(f[5],x[5],y[5]);
-	or(f[6],x[6],y[6]);
-	or(f[7],x[7],y[7]);
-	or(f[8],x[8],y[8]);
-	or(f[9],x[9],y[9]);
-	or(f[10],x[10],y[10]);
-	or(f[11],x[11],y[11]);
-	or(f[12],x[12],y[12]);
-	or(f[13],x[13],y[13]);
-	or(f[14],x[14],y[14]);
-	or(f[15],x[15],y[15]);
-	or(f[16],x[16],y[16]);
-	or(f[17],x[17],y[17]);
-	or(f[18],x[18],y[18]);
-	or(f[19],x[19],y[19]);
-	or(f[20],x[20],y[20]);
-	or(f[21],x[21],y[21]);
-	or(f[22],x[22],y[22]);
-	or(f[23],x[23],y[23]);
-	or(f[24],x[24],y[24]);
-	or(f[25],x[25],y[25]);
-	or(f[26],x[26],y[26]);
-	or(f[27],x[27],y[27]);
-	or(f[28],x[28],y[28]);
-	or(f[29],x[29],y[29]);
-	or(f[30],x[30],y[30]);
-	or(f[31],x[31],y[31]);
+	or #6(f[0],x[0],y[0]);
+	or #6(f[1],x[1],y[1]);
+	or #6(f[2],x[2],y[2]);
+	or #6(f[3],x[3],y[3]);
+	or #6(f[4],x[4],y[4]);
+	or #6(f[5],x[5],y[5]);
+	or #6(f[6],x[6],y[6]);
+	or #6(f[7],x[7],y[7]);
+	or #6(f[8],x[8],y[8]);
+	or #6(f[9],x[9],y[9]);
+	or #6(f[10],x[10],y[10]);
+	or #6(f[11],x[11],y[11]);
+	or #6(f[12],x[12],y[12]);
+	or #6(f[13],x[13],y[13]);
+	or #6(f[14],x[14],y[14]);
+	or #6(f[15],x[15],y[15]);
+	or #6(f[16],x[16],y[16]);
+	or #6(f[17],x[17],y[17]);
+	or #6(f[18],x[18],y[18]);
+	or #6(f[19],x[19],y[19]);
+	or #6(f[20],x[20],y[20]);
+	or #6(f[21],x[21],y[21]);
+	or #6(f[22],x[22],y[22]);
+	or #6(f[23],x[23],y[23]);
+	or #6(f[24],x[24],y[24]);
+	or #6(f[25],x[25],y[25]);
+	or #6(f[26],x[26],y[26]);
+	or #6(f[27],x[27],y[27]);
+	or #6(f[28],x[28],y[28]);
+	or #6(f[29],x[29],y[29]);
+	or #6(f[30],x[30],y[30]);
+	or #6(f[31],x[31],y[31]);
 endmodule
 
 /*
@@ -489,8 +421,8 @@ module mux2to1(d0, d1, s0, f);
 	input d0, d1, s0;
 	output f;
 	
-	and(w17, d1, s0);
-	not(w15, s0);
-	and(w18, w15, d0);
-	or(f, w17, w18);
+	and #2(w17, d1, s0);
+	not #1(w15, s0);
+	and #2(w18, w15, d0);
+	or #6(f, w17, w18);
 endmodule
